50d54a466ca96e4cf6978bf56e6bcea8969ace8e
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2021-03-11 00:50:00 +0100

    Response Base Model

---
+++ AcctOpeningImageValidationAPI/Repository/ViewModel/ResponseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace AcctOpeningImageValidationAPI.Repository.Response
+{
+    public class ResponseViewModel<T>
+    {
+        /// <summary>
+        /// Status (value : true or false)
+        /// </summary>
+        private bool Status { get; set; }
+
+        /// <summary>
+        /// Response Message
+        /// </summary>
+        private string Message { get; set; }
+
+        /// <summary>
+        /// Response Data
+        /// Type of T
+        /// </summary>
+        private T Data { get; set; }
+
+        /// <summary>
+        /// Response Status Code
+        /// </summary>
+        private string StatusCode { get; set; }
+    }
+}
