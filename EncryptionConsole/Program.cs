761909af3f440fcbbbffd382ad52a624ffa24315
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-05-31 11:30:23 +0100

    Reference Fixed

--- EncryptionConsole/Program.cs
+++ EncryptionConsole/Program.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using AcctOpeningImageValidationAPI.Models;
+using IdentificationValidationLib.Models;
 using Newtonsoft.Json;
 
 namespace EncryptionConsole
