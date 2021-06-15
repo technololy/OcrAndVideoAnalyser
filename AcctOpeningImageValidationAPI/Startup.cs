883e175f28d1070e81cf1213eae39064dbd25667
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-15 10:58:28 +0100

    Fixed Unconstructed Service -> IVerifyMeService

--- AcctOpeningImageValidationAPI/Startup.cs
+++ AcctOpeningImageValidationAPI/Startup.cs
@@ -61,6 +61,7 @@ namespace AcctOpeningImageValidationAPI
             services.AddScoped<IFaceRepository, FaceRepository>();
             services.AddScoped<RestClientService, RestClientService>();
             services.AddScoped<INetworkService, NetworkService>();
+            services.AddScoped<IVerifyMeService, IVerifyMeService>();
             // Register the Swagger generator, defining 1 or more Swagger documents
 
             services.AddSwaggerGen();
